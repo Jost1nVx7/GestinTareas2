20bfec5aaef6c9610e5ce931dfbafb1655b44f2f
JhxnierCT <jhonier.cotaya02@gmail.com>
2025-02-07 15:28:09 -0500

    Subir

---
+++ src/main/java/ec/edu/espoch/gestortarea/GestorTarea.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.gestortarea;
+
+/**
+ *
+ * @author SO-LAB-PC5
+ */
+public class GestorTarea {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
